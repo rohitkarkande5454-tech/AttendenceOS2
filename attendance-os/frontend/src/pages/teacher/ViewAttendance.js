71b8af122e819ca925d04068b05e72cf1c615726
rohitkarkande5454-tech <rohitkarkande5454@gmail.com>
2026-04-21 13:02:52 +0530

    Update axios import path in ViewAttendance.js

--- attendance-os/frontend/src/pages/teacher/ViewAttendance.js
+++ attendance-os/frontend/src/pages/teacher/ViewAttendance.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import axios from '../../api';
 import { useAuth } from '../../context/AuthContext';
 
 export function ViewAttendance() {

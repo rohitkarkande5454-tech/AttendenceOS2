e1769f29800b4ad3b68d89e8b6a997ca1a8b2631
rohitkarkande5454-tech <rohitkarkande5454@gmail.com>
2026-04-21 13:04:29 +0530

    Update StudentAttendance.js

--- attendance-os/frontend/src/pages/student/StudentAttendance.js
+++ attendance-os/frontend/src/pages/student/StudentAttendance.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import axios from '../../api';
 import { useAuth } from '../../context/AuthContext';
 import { Line, Doughnut } from 'react-chartjs-2';
 import {

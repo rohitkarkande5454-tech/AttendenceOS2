8073548bca1cacc10c9aac2ba1a8a3383b95c959
rohitkarkande5454-tech <rohitkarkande5454@gmail.com>
2026-04-21 13:04:52 +0530

    Update StudentDashboard.js

--- attendance-os/frontend/src/pages/student/StudentDashboard.js
+++ attendance-os/frontend/src/pages/student/StudentDashboard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import axios from '../../api';
 import { useAuth } from '../../context/AuthContext';
 import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
